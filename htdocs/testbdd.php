f403d1a79db3a9bb887d7d90b6ef47af76d1d2c7
Antoine Daudenthun <adaudenthun@atecna.fr>
2021-06-17 15:15:15 +0200

    [L3] script test bdd

---
+++ htdocs/testbdd.php
@@ -0,0 +1,9 @@
+<?php
+
+// https://www.php.net/manual/fr/book.pdo.php
+// https://www.php.net/manual/fr/pdo.connections.php
+
+// Connexion a la base de donnee
+// SELECT
+// Create table
+// INSERT
