963c7f502b0ccdbc4ea9a9bc5276cb3252c7e443
paradixsoftware <jimmy.monborren@gmail.com>
2021-06-18 15:52:13 +0200

    Jimmy Monborren - basic commit

---
+++ htdocs/src/Controller/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+    public function home() : Response{
+        return $this->render("test.html.twig");
+    }
+}
