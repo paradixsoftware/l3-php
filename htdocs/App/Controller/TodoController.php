4a61e7b8d1ef78e59d6277203706a7913740464e
Antoine Daudenthun <adaudenthun@atecna.fr>
2021-06-18 10:19:58 +0200

    [L3] Add controller todo

---
+++ htdocs/App/Controller/TodoController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use GuzzleHttp\Client;
+
+class TodoController extends AbstractController
+{
+
+    public function home()
+    {
+        // Use guzzle call api todo
+        // https://jsonplaceholder.typicode.com/todos
+    }
+
+}
