963549587e48981b4b4947a84dcf5f85ab38ef6b
paradixsoftware <jimmy.monborren@gmail.com>
2021-07-08 10:12:25 +0200

    Jimmy Monborren - Form+Insert+viewing+error404+firewall avec Symfony

---
+++ htdocs/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\User\User;
+
+class UserFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+
+        $user->setPassword($this->passwordHasher->hashPassword(
+                $user,
+                 '7777dd4z'
+        ));
+    }
+}
